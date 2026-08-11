d4a23277c3b140f23e6c4bc04037199323ca4a9a
Robert <robert.schlegel@imev-mer.fr>
2026-08-11 11:29:56 +0200

    X11.R cleanup that was missed by nightly run.

--- func/X11.R
+++ func/X11.R
@@ -4,18 +4,15 @@
 
 # Libraries ---------------------------------------------------------------
 
-# list_of_packages <- c("plyr", "tidyverse", "ggpubr", "viridis", "doParallel", "zoo", "ggnewscale", "scales")
-# new.packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
-# if(length(new.packages)) install.packages(new.packages)
-# lapply(list_of_packages, require, character.only = TRUE)
-
 library(tidyverse)
 library(viridis)
 library(ggpubr)
 library(scales)
 library(ggnewscale)
 library(zoo)
-# library(doParalell)
+
+# The central functions
+source("func/util.R")
 
 # where_are_saved_X11_results = "~/RiOMar/output/FIXED_THRESHOLD/BAY_OF_BISCAY"
 # Zone= "BAY_OF_SEINE"
@@ -46,44 +43,6 @@ get_X11_data <- function(where_are_saved_X11_results, Zone, Data_source, sensor_
   return( X11_ts )
   
 }
-
-
-sec_axis_adjustement_factors <- function(var_to_scale, var_ref) {
-  
-  index_to_keep <- which(is.finite(var_ref))
-  var_ref <- var_ref[index_to_keep]
-  
-  index_to_keep <- which(is.finite(var_to_scale))
-  var_to_scale <- var_to_scale[index_to_keep]
-  
-  max_var_to_scale <- max(var_to_scale, na.rm = T) 
-  min_var_to_scale <- min(var_to_scale, na.rm = T) 
-  max_var_ref <- max(var_ref, na.rm = T) 
-  min_var_ref <- min(var_ref, na.rm = T) 
-  
-  diff_to_scale <- max_var_to_scale - min_var_to_scale
-  diff_to_scale <- ifelse(diff_to_scale == 0, 1 , diff_to_scale)
-  diff_ref <- max_var_ref - min_var_ref
-  diff <- diff_ref / diff_to_scale
-  
-  adjust <- (max_var_ref - max_var_to_scale*diff) 
-  
-  return(data.frame(diff = diff, adjust = adjust, operation = "scaled var = (var_to_scale * diff) + adjust",
-                    trans_axis_operation = "var_to_scale = {scaled_var - adjust} / diff)"))
-}
-
-
-ggplot_theme <-   function() {
-  theme(text = element_text(size=35, colour = "black"), #25
-        plot.title = element_text(hjust = 0.5, size = 55),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        panel.border = element_rect(linetype = "solid", fill = NA),
-        axis.text = element_text(size = 35, colour = "black"),
-        axis.title = element_text(size = 40, colour = "black"),
-        axis.text.x=element_text(angle=0),
-        axis.ticks.length=unit(.25, "cm"))}
 
 
 make_the_plot <- function(X11_data, type_of_signal) {
@@ -139,26 +98,6 @@ make_the_plot <- function(X11_data, type_of_signal) {
           panel.border = element_rect(linetype = "solid", fill = NA))
   
   return(the_plot)
-  
-}
-
-
-save_plot_as_png <- function(plot, name = c(), width = 14, height = 8.27, path, res = 150) {
-  
-  graphics.off()
-  
-  if (name %>% length() == 1) {
-    if (dir.exists(path) == FALSE) {dir.create(path, recursive = TRUE)}
-    path <- file.path(path, paste(name, ".png", sep = ""))
-  } else {
-    path <- paste(path, ".png", sep = "")
-  }
-  
-  if (grepl(pattern = ".png.png", path)) {path <- path %>% gsub(pattern = ".png.png", replacement = ".png", x = .)}
-  
-  png(path, width = width, height = height, units = "in", res = res)
-  print(plot)
-  dev.off()
   
 }
 
